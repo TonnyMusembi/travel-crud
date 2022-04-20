9704ffb597d1b6714d81b2e695740eddb90f1067
Tonny <musembitonnyj@gmail.com>
2022-04-20 14:01:48 +0300

    add

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::resource('api/games',GameController::class);
+//Route::resource('api/games',GameController::class);
