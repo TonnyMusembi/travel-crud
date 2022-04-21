654f24f44e830c851c9735804f0219241ff69f55
Tonny <musembitonnyj@gmail.com>
2022-04-21 11:15:09 +0300

    add

---
+++ app/Http/Controllers/PurchaseController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Purchase;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+
+class PurchaseController extends Controller
+{
+    public function index(){
+        return Purchase::all();
+    }
+
+    public function store( Request $request){
+        $validator = Validator::make($request->all(), [
+            'product_id' => 'required',
+            'product'  => 'required',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['status' => 422, 'errors' => $validator->errors()]);
+        } else {
+
+            Purchase::create([
+                'product_id' => $request->input('product_id'),
+                'product' => $request->input('product'),
+            ]);
+        }
+        return response()->json(['req' => $request]);
+
+    }
+    public function create($id){
+   return view ('create');
+    }
+}
