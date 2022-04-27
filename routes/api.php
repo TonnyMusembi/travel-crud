239a1f5f1c200d6c20f9de1f3244788a11319186
Tonny <musembitonnyj@gmail.com>
2022-04-27 13:20:19 +0300

    add

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,8 @@ use App\Http\Controllers\UssdController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TodoController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -35,3 +37,20 @@ Route::resource('purchase', PurchaseController::class);
 Route::resource('bets',BetController::class);
 Route::resource('ussd', UssdController::class);
 
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+
+});
+
+
+
+Route::controller(TodoController::class)->group(function () {
+    Route::get('todos', 'index');
+    Route::post('todo', 'store');
+    Route::get('todo/{id}', 'show');
+    Route::put('todo/{id}', 'update');
+    Route::delete('todo/{id}', 'destroy');
+});
